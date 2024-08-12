a2da6955efb00b09d253b76964e9ac380cd42c27
Damian <dkowalski.1997@gmail.com>
2024-08-12 22:06:46 +0200

    added button

--- apps/nx-chat/src/app/components/chat-input/chat-input.component.ts
+++ apps/nx-chat/src/app/components/chat-input/chat-input.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
@@ -16,7 +17,7 @@ import { InputTextModule } from 'primeng/inputtext';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   providers: [ClassBinder],
-  imports: [InputTextModule, FormsModule],
+  imports: [InputTextModule, FormsModule, ButtonModule],
 })
 export class ChatInputComponent {
   public textValue = signal<string>('');
