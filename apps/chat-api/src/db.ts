6c71f4a9225667d8f7782a6aa68ca8265a0e79a0
Damian <dkowalski.1997@gmail.com>
2024-08-22 19:20:40 +0200

    added pg

--- apps/chat-api/src/db.ts
+++ apps/chat-api/src/db.ts
@@ -1,1 +1,37 @@
-export class DatabaseController {}
+import { Client, QueryResult } from 'pg';
+
+export class DatabaseController {
+  private _client: Client | null = null;
+
+  get client(): Client {
+    const client = this._client;
+    if (!client) {
+      throw new Error('Client has not been initialized!');
+    }
+
+    return client;
+  }
+
+  public async initialize(): Promise<void> {
+    this._client = new Client({
+      host: 'localhost',
+      user: 'root',
+      password: '123',
+      port: 5432,
+      database: 'nx-chat',
+    });
+
+    await this._client.connect();
+    await this._seedDb();
+  }
+
+  private _seedDb(): Promise<QueryResult<any>> {
+    return this.client.query(
+      `CREATE TABLE IF NOT EXISTS messages (
+          message_text TEXT,
+          x_pos INTEGER,
+          y_pos INTEGER
+      );`
+    );
+  }
+}
--- apps/chat-api/src/main.ts
+++ apps/chat-api/src/main.ts
@@ -1,4 +1,10 @@
+import { DatabaseController } from './db';
 import { WebSocketController } from './ws';
 
 const wsController = new WebSocketController();
 wsController.initialize();
+
+const dbController = new DatabaseController();
+dbController.initialize().then(() => {
+  console.log('DB CONNECTED SUCCESSFULLY');
+});
